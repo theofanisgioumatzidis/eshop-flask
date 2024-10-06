1e95dad9ba1d08eb8909bd236da74a92e9875770
theofanisgioumatzidis <108617392+theofanisgioumatzidis@users.noreply.github.com>
2024-10-07 00:35:45 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,14 +1,9 @@
-import csv
-import datetime
-import pytz
-import requests
-import urllib
-import uuid
+
 import sqlite3
 
-from flask import redirect, render_template, request, session, flash
+from flask import redirect, render_template, session, flash
 from functools import wraps
-import requests
+
 
 
 DATABASE = ""
@@ -58,7 +53,6 @@ def apology(message, code=400):
 
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
-
 def login_required(f):
     """
     Decorate routes to require login.
@@ -71,43 +65,11 @@ def login_required(f):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
+
     return decorated_function
 
 
-def lookup(symbol):
-    """Look up quote for symbol."""
-
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
-    try:
-        response = requests.get(
-            url,
-            cookies={"session": str(uuid.uuid4())},
-            headers={"Accept": "*/*", "User-Agent": request.headers.get("User-Agent")},
-        )
-        response.raise_for_status()
-
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        price = round(float(quotes[-1]["Adj Close"]), 2)
-        return {"price": price, "symbol": symbol}
-    except (KeyError, IndexError, requests.RequestException, ValueError):
-        return None
-
-
 def euro(value):
-    """Format value as USD."""
+    """Format value as euro."""
     return f"{value:,.2f}€"
 
