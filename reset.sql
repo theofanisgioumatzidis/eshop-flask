4fa25f2adbe349f796320c9c4f6a1338fd8a2845
theofanisgioumatzidis <108617392+theofanisgioumatzidis@users.noreply.github.com>
2024-10-07 00:35:17 +0300

    Update reset.sql

--- reset.sql
+++ reset.sql
@@ -1,5 +1,9 @@
   DROP TABLE IF EXISTS cart;
   DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS products;
+  DROP TABLE IF EXISTS cart;
+  DROP TABLE IF EXISTS orders;
+  DROP TABLE IF EXISTS orderss_products;
 CREATE TABLE users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username TEXT NOT NULL,
@@ -10,7 +14,7 @@ INSERT INTO users (username, hash, access)
        VALUES ("admin", "scrypt:32768:8:1$py2xjAE75jftzhz9$8d51f666de58e2a9badf899beb900bfb30d40184f918b98574f53e00f34d36f4a866fc8e25cfc14db86e25c59377d6ca33c38fa5903e6c563fba0d3a9dca22dd", "admin");
 
 
-  DROP TABLE IF EXISTS products;
+
 CREATE TABLE products (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         title TEXT NOT NULL,
@@ -34,12 +38,28 @@ INSERT INTO products (title, brand, price, descript,
              "Epic moments are now accessible to everyone. The Galaxy S23 FE opens the door for more people to experience the extraordinary. With long-lasting power and stunning night shots, the phone becomes your gateway to epic memories that last.",
              "/static/product_img/sam_s23_fe_256.jpeg", 8, 128, "android", 2023, "black");
 
-  DROP TABLE IF EXISTS cart;
+
 CREATE TABLE cart (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user_id INTEGER NOT NULL,
                     product_id INTEGER NOT NULL,
                     quantity INTEGER NOT NULL,
-                    FOREIGN KEY (user_id) REFERENCES users(id),
+                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+                    );
+
+CREATE TABLE orders (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER NOT NULL,
+                    the_date TEXT NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                    );
+
+CREATE TABLE orderss_products (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    order_id INTEGER NOT NULL,
+                    product_id INTEGER NOT NULL,
+                    quantity INTEGER NOT NULL,
+                    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
                     FOREIGN KEY (product_id) REFERENCES products(id)
                     );
