5f0a650fece12ccfb1a877fd5225633945de4729
theofanisgioumatzidis <108617392+theofanisgioumatzidis@users.noreply.github.com>
2024-10-09 18:03:22 +0300

    Update reset.sql

--- reset.sql
+++ reset.sql
@@ -2,7 +2,6 @@
   DROP TABLE IF EXISTS users;
   DROP TABLE IF EXISTS products;
   DROP TABLE IF EXISTS cart;
-  DROP TABLE IF EXISTS orders;
   DROP TABLE IF EXISTS orderss_products;
 CREATE TABLE users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,6 +16,8 @@ INSERT INTO users (username, hash, access)
 
 CREATE TABLE products (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        active TEXT NOT NULL,
+                        category TEXT NOT NULL,
                         title TEXT NOT NULL,
                         brand TEXT NOT NULL,
                         price FLOAT NOT NULL,
@@ -29,14 +30,33 @@ CREATE TABLE products (
                         color TEXT
                     );
 
-INSERT INTO products (title, brand, price, descript,
+INSERT INTO products (active, category, title, brand, price, descript,
                         img_route, ram, memory, os_system, release, color)
-       VALUES ("iPhone 15 5G (6GB/128GB)", "apple", 799.00,
+       VALUES ("True", "Smartphones","iPhone 15 5G (6GB/128GB)", "apple", 799.00,
              "iPhone 15 offers Dynamic Island, a 48MP main camera and a USB-C port — all in a durable design that combines aluminum and color-infused glass.",
              "/static/product_img/iphone_15_128.jpeg", 6, 128, "ios", 2023, "black"),
-            ("Samsung Galaxy S23 FE (Exynos) 5G (8GB/128GB)", "samsung", 488.88,
+            ("True", "Smartphones","Samsung Galaxy S23 FE (Exynos) 5G (8GB/128GB)", "samsung", 488.88,
              "Epic moments are now accessible to everyone. The Galaxy S23 FE opens the door for more people to experience the extraordinary. With long-lasting power and stunning night shots, the phone becomes your gateway to epic memories that last.",
-             "/static/product_img/sam_s23_fe_256.jpeg", 8, 128, "android", 2023, "black");
+             "/static/product_img/sam_s23_fe_256.jpeg", 8, 128, "android", 2023, "black"),
+             ("True", "Laptops","Apple MacBook Pro 16", "Apple", 3790.00,
+             "MacBook Pro is for professionals looking for maximum power in areas like video and audio processing, as well as professional developers.",
+             "/static/product_img/Apple_MacBook_Pro_16.jpeg", 32, 1000, "IOS", 2021, "Silver"),
+             ("True", "Laptops","Lenovo ThinkPad P16v Gen 2 (Intel) 16", "Lenovo", 3063.00,
+             "If you want a workstation that's great performance but light and thin at the same time, the Thinkpad P16v Gen2 is the perfect laptop for you.",
+             "/static/product_img/Lenovo_ThinkPad_P16v_Gen_2_(Intel).jpeg", 64, 1000, "Windows", 2024, "Black"),
+             ("True", "Laptops","Dell XPS 16 9640 16.3", "Dell", 4888.00,
+             "Run demanding applications more smoothly and faster with powerful Intel ® Core™ Ultra 9 processors. Performance-class Intel ® Core™ Ultra processors provide a dedicated engine to unlock AI capabilities on the PC.",
+             "/static/product_img/Dell_XPS_16_9640_16.3.jpeg", 64, 2000, "Windows", 2024, "Silver"),
+             ("True", "Laptops","Razer Blade 15", "Razer", 2222.00,
+             "With Razer Blade 15, the real power will always be where you are. Featuring the latest 13th Gen Intel® Core™ i7 processors, NVIDIA® GeForce RTX™ 40 Series graphics, and a stunning 240Hz QHD display, enjoy unrivalled performance packed into one of the thinnest 15 RTX gaming laptop bezels ever made.",
+             "/static/product_img/Razer_Blade_15.jpeg", 16, 1000, "Windows", 2023, "Black"),
+             ("True", "Smartphones","Xiaomi 14 Ultra 5G", "Xiaomi", 1140.00,
+             "The Xiaomi 14 Ultra adopts a brand new integrated 6M42 high-strength aluminum frame material, with the metal frame extending to the back of the frame, doubling the robustness compared to the Xiaomi 13 Pro.",
+             "/static/product_img/Xiaomi_14_Ultra_5G.jpeg", 16, 512, "Android", 2023, "Black"),
+             ("True", "Smartphones","Google Pixel 9 Pro XL 5G", "Google", 1140.00,
+             "The Pixel 9 Pro XL's overhead new design features a silky matte glass back, a beautiful double-finished camera bar, and a polished metal frame that's as good as it looks.",
+             "/static/product_img/Google_Pixel_9_Pro_XL_5G.jpeg", 16, 256, "Android", 2023, "Obsidian");
+
 
 
 CREATE TABLE cart (
@@ -48,10 +68,13 @@ CREATE TABLE cart (
                     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
                     );
 
+  DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     user_id INTEGER NOT NULL,
                     the_date TEXT NOT NULL,
+                    shipping_cost REAL NOT NULL,
+                    total_cost REAL NOT NULL,
                     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
                     );
 
